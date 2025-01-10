89ffed86248bf5a0482758bfc01df8ee773bacf7
Unknown-Coder <ninjanigeljoe@gmail.com>
2025-01-10 11:10:38 -0500

    added containers

--- UI/Design.cs
+++ UI/Design.cs
@@ -1,7 +1,9 @@
-﻿using SpectralWave.Utill;
+using SpectralWave.Modules.Misc;
+using SpectralWave.Utill;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static SpectralWave.UI.SpectralUI;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -45,11 +47,45 @@ namespace SpectralWave.UI.Managment
             GUILayout.Label($"<size=18><b>{text}</b></size>");
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
-        } 
+        }
+        public static void CreateBox(string title, System.Action Act)
+        {
+            Rect BoxRect = new Rect(160f, 50f, 222f, 240f);
+            DrawTexture.DrawTextureRounded(BoxRect, BoxTexture, ScaleMode.StretchToFill, true, 1f, Color.white, Vector4.zero, new Vector4(4f, 4f, 4f, 4f));
+
+            GUI.Label(new Rect(BoxRect.x + 10f, BoxRect.y + 2, BoxRect.width - 20f, 30f), "<size=16><b>" + title + "</b></size>");
+
+            GUILayout.BeginArea(new Rect(BoxRect.x + 10f, BoxRect.y + 30f, BoxRect.width - 20f, BoxRect.height - 40f));
+            if (Act != null) Act();
+            GUILayout.EndArea();
+        }
+
+
+        public static void CreateRightBox(string title, System.Action Act)
+        {
+            Rect RightBoxRect = new Rect(390f, 50f, 250f, 240f);
+            DrawTexture.DrawTextureRounded(RightBoxRect, BoxTexture, ScaleMode.StretchToFill, true, 1f, Color.white, Vector4.zero, new Vector4(4f, 4f, 4f, 4f));
+            GUI.Label(new Rect(RightBoxRect.x + 10f, RightBoxRect.y + 2, RightBoxRect.width - 20f, 30f), "<size=16><b>" + title + "</b></size>");
+            GUILayout.BeginArea(new Rect(RightBoxRect.x + 10f, RightBoxRect.y + 30f, RightBoxRect.width - 20f, RightBoxRect.height - 40f));
+            Act();
+            GUILayout.EndArea();
+        }
+
+        /*  ONLY USIING IF THERE IS NO RIGHT BOX!!!!  */
+        public static void CreateLeftBox(string title, System.Action Act)
+        {
+            Rect LeftBoxRect = new Rect(160f, 50f, 300f, 250f);
+            DrawTexture.DrawTextureRounded(LeftBoxRect, BoxTexture, ScaleMode.StretchToFill, true, 1f, Color.white, Vector4.zero, new Vector4(4f, 4f, 4f, 4f));
+            GUI.Label(new Rect(LeftBoxRect.x + 10f, LeftBoxRect.y + 2, LeftBoxRect.width - 20f, 20f), "<size=16><b>" + title + "</b></size>");
+            GUILayout.BeginArea(new Rect(LeftBoxRect.x + 20f, LeftBoxRect.y + 30f, LeftBoxRect.width - 20f, LeftBoxRect.height - 40f));
+            if (Act != null) Act();
+            GUILayout.EndArea();
+        }
+
         public static bool CreateToggle(bool value, string text)
         {
             GUILayout.BeginHorizontal();
-            Rect rect = GUILayoutUtility.GetRect(28, 27, GUILayout.ExpandWidth(false));
+            Rect rect = DrawTexture.GetRect(28, 27, GUILayout.ExpandWidth(false));
             DrawTexture.DrawTextureRounded(rect, value ? activebuttonTexture : normalToggleTexture, ScaleMode.StretchToFill, true, 1f, Color.white, Vector4.zero, new Vector4(4f, 4f, 4f, 4f));
             if (GUI.Button(new Rect(rect.x + 5, rect.y + 6f, 25f, 29f), "", GUIStyle.none)) value = !value;
 
@@ -58,31 +94,30 @@ namespace SpectralWave.UI.Managment
             GUILayout.Space(3f);
             return value;
         }
-        public static void CreateButton(string text, System.Action action)
+        public static void CreateButton(string text, System.Action Act)
         {
-            Rect rect = GUILayoutUtility.GetRect(320, 40);
+            Rect rect = DrawTexture.GetRect(320, 40);
             GUILayout.BeginHorizontal();
-            Rect buttonRect = new Rect(rect.x + 210, rect.y + 6f, 75, 25f);
+            Rect buttonRect = new Rect(rect.x + 5, rect.y + 6f, 75, 25f); 
             DrawTexture.DrawTextureRounded(buttonRect, normalbuttonTexture, ScaleMode.StretchToFill, true, 1f, Color.white, Vector4.zero, new Vector4(6, 6, 6, 6));
-            if (GUI.Button(buttonRect, "<b>Execute</b>", new GUIStyle { alignment = TextAnchor.MiddleCenter, normal = { textColor = Color.white } })) action?.Invoke();
-            GUI.Label(new Rect(rect.x + 35, rect.y + 7f, 1000, 24), $"<b>{text}</b>");
+            if (GUI.Button(buttonRect, "<b>Execute</b>", new GUIStyle { alignment = TextAnchor.MiddleCenter, normal = { textColor = Color.white } })) Act?.Invoke();
+            GUI.Label(new Rect(rect.x + 85, rect.y + 7f, 1000, 24), $"<b>{text}</b>"); 
             GUILayout.EndHorizontal();
             GUILayout.Space(3f);
         }
 
-        public static float SlideWidth = 190;
 
+        public static float SlideWidth = 190;
         public static void CreateSlider(ref float value, float minValue, float maxValue, string label)
         {
-            float filledWidth = SlideWidth * (value - minValue) / (maxValue - minValue);
-            Rect rect = GUILayoutUtility.GetRect(320, 40);
-
-            DrawTexture.DrawTextureRounded(new Rect(rect.x + 5, rect.y + 7f, SlideWidth, 18), SliderTexture, ScaleMode.StretchToFill, true, 1, Color.white, new Vector4(0, 0, 0, 0), new Vector4(9, 9, 9, 9));
-            DrawTexture.DrawTextureRounded(new Rect(rect.x + 5 + Mathf.Clamp(filledWidth - 10, 0, SlideWidth - 20), rect.y + 7f, 20, 20), SliderBcTexture, ScaleMode.StretchToFill, true, 1, Color.white, new Vector4(0, 0, 0, 0), new Vector4(10, 10, 10, 10));
-
-            value = GUI.HorizontalSlider(new Rect(rect.x + 5, rect.y + 7f, SlideWidth, 16.5f), value, minValue, maxValue, GUIStyle.none, GUIStyle.none);
-            GUI.Label(new Rect(rect.x + 210, rect.y + 7f, 100, 24), $"<b>{label}: {Mathf.Round(value)}</b>");
+            float w = 120f, f = w * (value - minValue) / (maxValue - minValue);
+            Rect r = DrawTexture.GetRect(320, 40);
+            DrawTexture.DrawTextureRounded(new Rect(r.x + 5, r.y + 7f, w, 18), SliderTexture, ScaleMode.StretchToFill, true, 1, Color.white, Vector4.zero, new Vector4(9, 9, 9, 9));
+            DrawTexture.DrawTextureRounded(new Rect(r.x + 5 + Mathf.Clamp(f - 10, 0, w - 20), r.y + 7f + 6, 20, 6), SliderBcTexture, ScaleMode.StretchToFill, true, 1, Color.white, Vector4.zero, Vector4.zero);
+            value = GUI.HorizontalSlider(new Rect(r.x + 5, r.y + 7f, w, 16.5f), value, minValue, maxValue, GUIStyle.none, GUIStyle.none);
+            GUI.Label(new Rect(r.x + 130, r.y + 7f, 100, 24), $"<b>{label}: {Mathf.Round(value)}</b>");
             GUILayout.Space(3f);
         }
+
     }
 }
